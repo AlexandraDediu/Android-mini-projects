0c235cf59e61cf6683dc7ef6ddec8f6567d7048e
Alexandra Dediu <alexandradediu266@gmail.com>
2020-03-19 20:51:19 +0200

    week9

---
+++ week9/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DataSource dataSource = new DataSource(this);
+        dataSource.getServerDataSource(this);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    public void setNamesList(List<String> nameList) {
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(nameList);
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+}
