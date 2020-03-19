d673d6cdb7ab1b0483ad4178106491c22757b932
Alexandra Dediu <alexandradediu266@gmail.com>
2020-03-19 20:49:06 +0200

    week8

---
+++ week8/src/main/java/com/example/myapplication/RetrofitApi.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface RetrofitApi {
+    @GET("mariusgherman/androidfundamentals/weeks/week8/test_json1")
+    Call<List<Person>> getPersons();
+
+}
